761be2eb5c42ea0abbab37cad3be7ddeb05a0e82
Bert Carels <bert.carels@euroclear.com>
2024-06-06 21:02:33 +0000

    Updated build.sh

--- cicd/build.sh
+++ cicd/build.sh
@@ -8,6 +8,6 @@ fi
 set
 echo $GITVERSION_FULLSEMVER
 
-buildah build --build-context src=../src -t javawhlpub:$1 --platform linux/amd64 .
+buildah build --env GITVERSION_FULLSEMVER --build-context src=../src -t javawhlpub:$1 --platform linux/amd64 .
 ####docker tag javawhlpub:$1 bcacr2023.azurecr.io/kab-whl/javawhlpub:$1
 ####docker push bcacr2023.azurecr.io/kab-whl/javawhlpub:$1
